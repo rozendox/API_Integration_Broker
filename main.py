7d88cb5967156af3e76867123a56a673beee776c
Gabriel-Rozendo <gabrielsg236@gmail.com>
2026-06-02 15:18:59 -0400

    feat: inicializando esturutura basica e adicionando main.py com função de exemplo

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+docstring for main.py
+
+This module contains the main function to execute the program.
+"""
+
+def main():
+    """
+    Main function to execute the program.
+    """
+    print("Hello, World!")
